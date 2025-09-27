d1d4caab78f3b5edb55a01fae597fea3d4c3d485
sigonebyexample <hosseinnansaei@gmail.com>
2025-09-27 05:17:51 +0330

    Create Code0.py

---
+++ beginner/Code0.py
@@ -0,0 +1,7 @@
+# Assume 3 variables are already defined for you: a, b, and c. Create a variable called total that adds a and b then multiplies the result by c. Include a last line in your code to print the value: print(total)
+a=12
+b=25
+c=56
+total=(a+b)
+total=total*c
+print(total) 
