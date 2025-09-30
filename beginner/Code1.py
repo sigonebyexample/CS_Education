fccbacbf0a03387c1d1c1ad9a32cf2203118c91d
sigonebyexample <hosseinnansaei@gmail.com>
2025-09-30 13:57:26 +0330

    Update Code1.py

--- beginner/Code1.py
+++ beginner/Code1.py
@@ -1,1 +1,4 @@
-#In here I uploade my second code in this major
+text=input("Enter a verb : ") 
+print(f"I can { text } better than you!\n",((text+" ")*5))
+#Output for example: I can run better than you!
+# run run run run run 
