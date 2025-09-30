3e804259beddbee91b5ae4d8c87c1e195859e03c
sigonebyexample <hosseinnansaei@gmail.com>
2025-09-30 14:03:24 +0330

    Update Code1.py

--- beginner/Code1.py
+++ beginner/Code1.py
@@ -1,4 +1,4 @@
 text=input("Enter a verb : ") 
-print(f"I can { text } better than you!\n",((text+" ")*5))
+print(f"I can { text } better than you!\n",((text+" ")*4)+text)
 #Output for example: I can run better than you!
 # run run run run run 
