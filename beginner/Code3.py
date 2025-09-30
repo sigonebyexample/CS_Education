4c8b96ccc880b4e8c81f2e0727cd626ed65842b2
sigonebyexample <hosseinnansaei@gmail.com>
2025-10-01 00:18:59 +0330

    Create Code3.py

---
+++ beginner/Code3.py
@@ -0,0 +1,7 @@
+x=int(input("enter any number : "))
+if x==0:
+    print("You entered zero")
+elif x>0:
+    print("You entered a positive number")
+else:
+    print("You entered a negative number")
