4a705b67036c061061221015e81e9dee6bdf4841
sigonebyexample <hosseinnansaei@gmail.com>
2025-10-06 22:40:09 +0330

    Update Code7.py

--- beginner/Code7.py
+++ beginner/Code7.py
@@ -1,1 +1,25 @@
-Just for coming 
+for i in range(5):
+    if (i!=0):
+        if (i%2==0):
+            print(i)
+print("Done")
+for i in range(10):
+    if (i!=0):
+        if (i%2==0):
+            print(i)
+print("Done")
+for i in range(2,9,3):
+    if (i!=0):
+        if (i%2==0):
+            print(i)
+print("Done")
+for i in range(-4,6,2):
+    if (i!=0):
+        if (i%2==0):
+            print(i)
+print("Done")
+for i in range(5,6):
+    if (i!=0):
+        if (i%2==0):
+            print(i)
+print("Done")
